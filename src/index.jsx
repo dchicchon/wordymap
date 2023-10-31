4f7e7305a8b8cda5c524d16355cd00981e1db1ca
Daniel Chicchon <50509869+dchicchon@users.noreply.github.com>
2023-10-31 12:20:52 -0700

    Update index.jsx
    Testing commits from mobile

--- src/index.jsx
+++ src/index.jsx
@@ -9,6 +9,7 @@ import { useEffect } from 'react';
 
 const App = () => {
   useEffect(() => {
+    console.log('hello from ios')
     console.log(window.innerWidth);
     // const cellWidth = window.innerWidth / 32;
     // console.log({ cellWidth });
