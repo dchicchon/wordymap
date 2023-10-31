e499e2d4613144daa95d51ed23fa63d862aed132
Daniel Chicchon <50509869+dchicchon@users.noreply.github.com>
2023-10-31 12:28:01 -0700

    Update index.jsx
    Remove console logs and useEffect

--- src/index.jsx
+++ src/index.jsx
@@ -2,18 +2,11 @@ import TileRack from './components/TileRack';
 import TilesBoard from './components/TilesBoard';
 import Navbar from './components/Navbar/Navbar';
 import styles from './index.module.css';
-import { useEffect } from 'react';
 
 // we should check the width of the screen were currently on
 // with that width, we can create the tilesizes on the board
 
 const App = () => {
-  useEffect(() => {
-    console.log('hello from ios')
-    console.log(window.innerWidth);
-    // const cellWidth = window.innerWidth / 32;
-    // console.log({ cellWidth });
-  }, []);
   return (
     <div className={styles.App}>
       <Navbar />
